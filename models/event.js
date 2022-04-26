267e19372270fc47548950ef194d651ba4dd0fda
Christina <christina.hurt@purplewave.com>
2022-04-25 20:46:42 -0500

    sequelize associations

--- models/event.js
+++ models/event.js
@@ -9,10 +9,30 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Stage, StageEvent, MeetGreets, SetTimes }) {
       // define association here
-    }
+      // stages
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages", 
+        through: StageEvent
+        }),
+      // meet and greets
+      Event.hasMany(MeetGreets, {
+        foreignKey: "event_id",
+        as: "meet_greets"
+        }),
+      // meet and greets
+      Event.hasMany(SetTimes, {
+        foreignKey: "event_id",
+        as: "set_times"
+        })
+      }
   }
+    
+
+
+
   Event.init({
     event_id: {
         allowNull: false,
--- models/meetGreet.js
+++ models/meetGreet.js
@@ -9,8 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Band, Event }) {
       // define association here
+      // band
+      MeetGreet.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band"
+      }), 
+
+      // event
+      MeetGreet.belongsTo(Event, {
+        foreignKey: "meet_greet_id",
+        as: "event"
+      })
     }
   }
   MeetGreet.init({
